802b22564b9f350013b34642e5369ff7a7296b1b
jorge ochoa <jorge.uchija2021@gmail.com>
2024-07-15 22:53:11 -0400

    scrolling de modal show

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -2,10 +2,11 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   connect() {
+    /*
     this.element.addEventListener("turbo:submit-end", (event) => {
       if (event.detail.success) {
         Turbo.visit(event.detail.fetchResponse.response.url);
       }
-    });
+    });*/
   }
 }
