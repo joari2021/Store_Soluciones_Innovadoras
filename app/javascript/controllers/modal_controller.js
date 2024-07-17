67b63e14d0785bb1cd9d31c95dd0e739eb81a5cc
jorge ochoa <jorge.uchija2021@gmail.com>
2024-07-16 21:56:25 -0400

    ajuste turbo event

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -4,7 +4,11 @@ export default class extends Controller {
   connect() {
     this.element.addEventListener("turbo:submit-end", (event) => {
       if (event.detail.success) {
+        console.log(event.detail.fetchResponse);
         Turbo.visit(event.detail.fetchResponse.response.url);
+        if (event.detail.fetchResponse.response.redirected == true) {
+          //Turbo.visit(event.detail.fetchResponse.response.url);
+        }
       }
     });
   }
