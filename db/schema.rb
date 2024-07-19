d0dc1a4e840a48c2a85b192d427e947282e9671c
jorge ochoa <jorge.uchija2021@gmail.com>
2024-07-18 22:12:06 -0400

    Creacion de modelo User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_13_233548) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_19_020528) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -157,6 +157,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_13_233548) do
     t.datetime "updated_at", null: false
     t.boolean "disponible", default: true
     t.string "link_trailer"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "username", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
