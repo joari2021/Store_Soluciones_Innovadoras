acd6ee7e49a9fba88d4e9cd804c909b5bde688b5
jorge ochoa <jorge.uchija2021@gmail.com>
2024-07-23 21:44:53 -0400

    Desarrollo Responsive de peliculas

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,22 @@ import "controllers";
 
 // app/javascript/application.js
 import "bootstrap";
+
+let loadMoreBtn1 = document.querySelector("#load-more-1");
+let currentItem1 = 4;
+
+loadMoreBtn1.onclick = () => {
+  let boxes = [...document.querySelectorAll(".box-container-1 .box-1")];
+  // Asegúrate de que el índice no sea mayor que el número de elementos disponibles
+  let nextItems = Math.min(currentItem1 + 4, boxes.length);
+
+  for (let i = currentItem1; i < nextItems; i++) {
+    boxes[i].style.display = "inline-block";
+  }
+
+  currentItem1 = nextItems;
+
+  if (currentItem1 >= boxes.length) {
+    loadMoreBtn1.style.display = "none";
+  }
+};
