45d336393d105b5f7881016435cf7a6c6974aefd
jorge ochoa <jorge.uchija2021@gmail.com>
2024-07-26 07:56:41 -0400

    Nav con opciones desplegables. Parte I

--- app/javascript/controllers/nav_controller.js
+++ app/javascript/controllers/nav_controller.js
@@ -21,5 +21,22 @@ export default class extends Controller {
         this.element.classList.remove("show-menu");
       });
     }
+
+    /* Apertura y cierre de listas dinamicas */
+    let listElements = document.querySelectorAll(".list__button--click");
+
+    listElements.forEach((listElement) => {
+      listElement.addEventListener("click", () => {
+        listElement.classList.toggle("arrow");
+
+        let height = 0;
+        let menu = listElement.nextElementSibling;
+        if (menu.clientHeight == "0") {
+          height = menu.scrollHeight;
+        }
+
+        menu.style.height = `${height}px`;
+      });
+    });
   }
 }
