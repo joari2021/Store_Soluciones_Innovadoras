fd41f47f5f056bbd942090eb95745c69e2421a32
Jorge Ochoa <jorge.uchija2021@gmail.com>
2025-04-21 17:27:33 -0400

    link edit y precio sugerido total

--- app/controllers/productos_controller.rb
+++ app/controllers/productos_controller.rb
@@ -8,6 +8,71 @@ class ProductosController < ApplicationController
     if params[:query_text].present?
       @productos = @productos.whose_name_starts_with(params[:query_text])
     end
+=begin
+    # Filtrar productos con precios que necesitan corrección
+    if params[:filter] == "alerta"
+      @productos = @productos.select do |producto|
+        costo_unidad = producto.precio_costo / producto.cant_unidades
+        precio_sugerido_usd = producto.precio_sugerido_usd(costo_unidad)
+
+        if producto.moneda_base_precio == "Dolar" 
+          precio_sugerido_usd > producto.precio_venta_usd && Current.user&.admin? 
+        else
+          precio_sugerido_bs = producto.calcular_precio_bs(precio_sugerido_usd)
+          precio_sugerido_bs > producto.precio_venta_bs && Current.user&.admin? 
+        end
+      end
+    end
+
+    # Contar el total de productos con alerta (sin importar el filtro)
+    @total_alertas = Producto.all.select do |producto|
+      costo_unidad = producto.precio_costo / producto.cant_unidades
+      precio_sugerido_usd = producto.precio_sugerido_usd(costo_unidad)
+
+      if producto.moneda_base_precio == "Dolar"
+        precio_sugerido_usd > producto.precio_venta_usd
+      else
+        precio_sugerido_bs = producto.calcular_precio_bs(precio_sugerido_usd)
+        precio_sugerido_bs > producto.precio_venta_bs
+      end
+    end.size
+=end
+    # Mapeo de nivel_ganancia a valores numéricos
+    @nivel_ganancia_map = {
+      "Baja" => 20,
+      "Media" => 30,
+      "Alta" => 50
+    }
+    # Filtrar productos con alerta de precio si el filtro está activado
+    if params[:filter] == "alerta"
+      @productos = @productos.where(
+        "(moneda_base_precio = 'Dolar' AND precio_venta_usd < (precio_costo / cant_unidades) / (1 - CAST(CASE nivel_ganancia
+          WHEN 'Baja' THEN 20
+          WHEN 'Media' THEN 30
+          WHEN 'Alta' THEN 50
+        END AS float) / 100)) OR
+         (moneda_base_precio = 'Bolivar' AND precio_venta_bs < ((precio_costo / cant_unidades) / (1 - CAST(CASE nivel_ganancia
+          WHEN 'Baja' THEN 20
+          WHEN 'Media' THEN 30
+          WHEN 'Alta' THEN 50
+        END AS float) / 100)) * #{@tasa_dolar_bcv})"
+      )
+    end
+
+    # Contar el total de productos con alerta (sin importar el filtro)
+    @total_alertas = Producto.where(
+      "(moneda_base_precio = 'Dolar' AND precio_venta_usd < (precio_costo / cant_unidades) / (1 - CAST(CASE nivel_ganancia
+        WHEN 'Baja' THEN 20
+        WHEN 'Media' THEN 30
+        WHEN 'Alta' THEN 50
+      END AS float) / 100)) OR
+       (moneda_base_precio = 'Bolivar' AND precio_venta_bs < ((precio_costo / cant_unidades) / (1 - CAST(CASE nivel_ganancia
+        WHEN 'Baja' THEN 20
+        WHEN 'Media' THEN 30
+        WHEN 'Alta' THEN 50
+      END AS float) / 100)) * #{@tasa_dolar_bcv})"
+    ).count
+
     @pagy, @productos = pagy_countless(@productos, items: 24)
   end
 
