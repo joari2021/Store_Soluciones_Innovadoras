9471e8183f7cb184ec303555356bb9719ffde393
Jorge Ochoa <jorge.uchija2021@gmail.com>
2025-05-30 09:49:33 -0400

    ajuste de funcion de copiado services

--- app/javascript/controllers/modal_service_controller.js
+++ app/javascript/controllers/modal_service_controller.js
@@ -34,7 +34,7 @@ export default class extends Controller {
           "#serviceModal .badge.bg-primary"
         );
         if (priceBadge) {
-          text += `*Precio:* ${priceBadge.textContent.trim()}\n\n`;
+          text += `*PRECIO:* ${priceBadge.textContent.trim()}\n\n`;
         }
 
         // 3. Cards (excepto pasos a seguir y datos requeridos)
@@ -55,8 +55,8 @@ export default class extends Controller {
               .replace(/<[^>]+>/g, "");
             bodyText = bodyText.replace(/\n+/g, "\n").trim();
 
-            // Si es "Nota" o "Tiempo de entrega", título en negrita seguido del contenido en cursiva, sin salto de línea
-            if (title === "Nota" || title === "Tiempo de Entrega") {
+            // Si es "NOTA" o "TIEMPO DE ENTREGA", título en negrita seguido del contenido en cursiva, sin salto de línea
+            if (title === "NOTA" || title === "TIEMPO DE ENTREGA") {
               cardTexts.push(`*${title}:* _${bodyText}_`);
             } else {
               cardTexts.push(`*${title}*\n${bodyText}`);
