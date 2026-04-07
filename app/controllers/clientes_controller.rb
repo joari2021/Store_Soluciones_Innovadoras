bf5a73c4bb2ad953e7d6786407a54036d55d95b1
Jorge Ochoa <jorge.uchija2021@gmail.com>
2026-04-07 00:04:00 -0400

    registro de descuentos por usuarios 2

--- app/controllers/clientes_controller.rb
+++ app/controllers/clientes_controller.rb
@@ -95,6 +95,9 @@ class ClientesController < ApplicationController
       :phone,
       :address,
       :benefits_config_json,
+      :general_product_discount_percent,
+      product_rules_rows: %i[product_id mode value],
+      service_fixed_price_rows: %i[service_id value],
     )
     build_cliente_attributes(permitted)
   end
@@ -108,7 +111,54 @@ class ClientesController < ApplicationController
   def build_cliente_attributes(permitted)
     attrs = permitted.to_h
     raw_benefits_json = attrs.delete("benefits_config_json")
-    attrs["benefits_config"] = Cliente.normalize_benefits_config(raw_benefits_json)
+
+    benefits_config = Cliente.normalize_benefits_config(raw_benefits_json)
+
+    general_discount = attrs.delete("general_product_discount_percent")
+    if general_discount.present?
+      benefits_config["general_product_discount_percent"] =
+        [[general_discount.to_d, 0.to_d].max, 100.to_d].min.round(2).to_f
+    end
+
+    product_rows = Array(attrs.delete("product_rules_rows"))
+    if product_rows.any?
+      product_rules = {}
+      product_rows.each do |row|
+        next unless row.is_a?(Hash)
+
+        product_id = row["product_id"].to_s.strip
+        mode = row["mode"].to_s.strip.downcase
+        value = row["value"].to_d
+        next if product_id.blank?
+        next unless %w[fixed percent].include?(mode)
+        next unless value.positive?
+
+        normalized_value = mode == "percent" ? [value, 100.to_d].min : value
+        product_rules[product_id] = {
+          "mode" => mode,
+          "value" => normalized_value.round(2).to_f,
+        }
+      end
+      benefits_config["product_rules"] = product_rules
+    end
+
+    service_rows = Array(attrs.delete("service_fixed_price_rows"))
+    if service_rows.any?
+      service_prices = {}
+      service_rows.each do |row|
+        next unless row.is_a?(Hash)
+
+        service_id = row["service_id"].to_s.strip
+        value = row["value"].to_d
+        next if service_id.blank?
+        next unless value.positive?
+
+        service_prices[service_id] = value.round(2).to_f
+      end
+      benefits_config["service_fixed_prices"] = service_prices
+    end
+
+    attrs["benefits_config"] = Cliente.normalize_benefits_config(benefits_config)
     attrs
   end
 end
