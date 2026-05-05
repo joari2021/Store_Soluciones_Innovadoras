0e3917d81e036b1e9f027b22ce60694fa87ed572
Jorge Ochoa <jorge.uchija2021@gmail.com>
2026-05-05 11:37:26 -0400

    Remove inactivity-based session timeout

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -1,12 +1,9 @@
 module Authentication
   extend ActiveSupport::Concern
 
-  INACTIVITY_TIMEOUT_SECONDS = 2.hours.to_i
-
   included do
     before_action :set_current_user
     before_action :protect_pages
-    before_action :enforce_session_timeout
 
     private
 
@@ -18,27 +15,6 @@ module Authentication
 
       session.delete(:user_id)
       session.delete(:business_id)
-    end
-
-    def enforce_session_timeout
-      return unless request.format.html? || request.format.turbo_stream?
-      return unless session[:user_id]
-      return if session_timeout_exempt_user?
-
-      last_seen_at = session[:last_seen_at].to_i
-      now = Time.current.to_i
-
-      if last_seen_at.positive? && (now - last_seen_at) > INACTIVITY_TIMEOUT_SECONDS
-        reset_session
-        redirect_to new_session_path, alert: "Tu sesion expiro por inactividad."
-        return
-      end
-
-      session[:last_seen_at] = now if Current.user.present?
-    end
-
-    def session_timeout_exempt_user?
-      Current.user.present? && Current.user.username.to_s.casecmp('max').zero?
     end
 
     def protect_pages
