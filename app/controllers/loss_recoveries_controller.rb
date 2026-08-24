196d7f837e8f3c0436f76cc27041ca8d266f46eb
jorge ochoa <jorge.uchija2021@gmail.com>
2026-08-24 09:09:49 -0400

    Add view link and invoice detail panel for loss recovery invoices

--- app/controllers/loss_recoveries_controller.rb
+++ app/controllers/loss_recoveries_controller.rb
@@ -153,6 +153,15 @@ class LossRecoveriesController < ApplicationController
   def recovery_invoices
     scope = current_business.recovery_invoices.order(occurred_at: :desc)
     @recovery_invoices = scope.limit(500)
+
+    selected_id = params[:invoice_id].to_i
+    @selected_recovery_invoice = if selected_id.positive?
+                                   current_business
+                                     .recovery_invoices
+                                     .includes(:user, recovery_invoice_items: %i[producto product_variation])
+                                     .find_by(id: selected_id)
+                                 end
+    @selected_recovery_invoice_items = @selected_recovery_invoice&.recovery_invoice_items&.order(:id) || []
   end
 
   def replenishments
